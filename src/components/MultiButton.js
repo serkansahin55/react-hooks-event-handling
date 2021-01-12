87458a73e100e2b9ce73ff010753b5eb782a28a3
serkansahin55 <ssahin55@outlook.com>
2021-01-12 09:52:18 -0500

    commit tree d3695c75c49ebab631ea2c48f788a57be17f8e08

--- src/components/MultiButton.js
+++ src/components/MultiButton.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 function MultiButton() {
-  function handleClick(number) {
-    console.log(`Button ${number} was clicked`);
+  function handleClick() {
+    console.log(`Button x was clicked`);
   }
 
   return (
     <div>
-      <button onClick={handleClick}>Button 1</button>
-      <button onClick={handleClick}>Button 2</button>
-      <button onClick={handleClick}>Button 3</button>
+      <button onClick={() => handleClick(1)}>Button 1</button>
+      <button onClick={() => handleClick(2)}>Button 2</button>
+      <button onClick={() => handleClick(3)}>Button 3</button>
     </div>
   );
 }
